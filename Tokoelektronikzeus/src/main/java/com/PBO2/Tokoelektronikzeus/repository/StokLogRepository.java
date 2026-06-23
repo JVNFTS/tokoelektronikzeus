a7ba5087c80bcd2bb859f233492fd32d96561438
JVNFTS <ftsjvn@gmail.com>
2026-06-24 01:35:14 +0700

    Update StokLogRepository.java

--- Tokoelektronikzeus/src/main/java/com/PBO2/Tokoelektronikzeus/repository/StokLogRepository.java
+++ Tokoelektronikzeus/src/main/java/com/PBO2/Tokoelektronikzeus/repository/StokLogRepository.java
@@ -3,6 +3,9 @@ package com.PBO2.Tokoelektronikzeus.repository;
 import com.PBO2.Tokoelektronikzeus.model.StokLog;
 import com.PBO2.Tokoelektronikzeus.model.StokLog.JenisMutasi;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,12 +14,21 @@ public interface StokLogRepository extends JpaRepository<StokLog, Long> {
 
     List<StokLog> findByBarang_KodeBarang(String kodeBarang);
 
+    List<StokLog> findByKodeBarangRef(String kodeBarangRef);
+
     List<StokLog> findByJenisMutasi(JenisMutasi jenisMutasi);
 
     List<StokLog> findByTanggalBetween(LocalDateTime dari, LocalDateTime sampai);
+
+    List<StokLog> findByKodeBarangRefAndTanggalBetween(
+            String kodeBarangRef, LocalDateTime dari, LocalDateTime sampai);
 
     List<StokLog> findByBarang_KodeBarangAndTanggalBetween(
             String kodeBarang, LocalDateTime dari, LocalDateTime sampai);
 
     void deleteByNoTransaksi(String noTransaksi);
+
+    @Modifying
+    @Query("UPDATE StokLog s SET s.barang = null WHERE s.barang.kodeBarang = :kode")
+    void detachBarang(@Param("kode") String kodeBarang);
 }
