744b04bc63daa0e30550b79dc6aa811c416a9d28
JVNFTS <ftsjvn@gmail.com>
2026-06-27 20:28:03 +0700

    Enhance BarangRepository with findByAktifTrue method
    Added method to find active Barang items.

--- Tokoelektronikzeus/src/main/java/com/PBO2/Tokoelektronikzeus/repository/BarangRepository.java
+++ Tokoelektronikzeus/src/main/java/com/PBO2/Tokoelektronikzeus/repository/BarangRepository.java
@@ -1,9 +1,11 @@
 package com.PBO2.Tokoelektronikzeus.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.PBO2.Tokoelektronikzeus.model.Barang;
+
+import java.util.List;
 
 public interface BarangRepository extends JpaRepository<Barang, String> {
 
+    List<Barang> findByAktifTrue();
 }
